70005e468b3f538845d6507db580847b8aaee585
Alnyz <katro.coplax@gmail.com>
2019-08-01 01:16:58 +0700

    Update change_content.py

--- examples/change_content.py
+++ examples/change_content.py
@@ -1,1 +1,52 @@
-from AsyncLine import *import asynciocl = LineNext("ios")cl.login(qr=True)print(cl.authToken)cl.auth.url("/P4")trigger = {	"changed":{}}@cl.poll.hooks(type=25, filters=Filters.command("change profile", separator="\n"))async def change_profile_picture(msg):	"""	This method to change your profile picture		Args used: path as string where image store	and then remove path after changing profile	"""	path = "LINE.png"	await cl.changeProfile(path=path, remove_path=True)	@cl.poll.hooks(type=25, filters=Filters.command("change group pict", prefix="", separator="\n"))async def change_profile_picture(msg):	"""	This method to change group picture		Args used: url as string this url support content stream	and then remove path after changing profile	"""	url = "url image content"	await cl.updateGroupPicture(msg.to, url=path, remove_path=True)@cl.poll.hooks(type=25, filters=Filters.command("change pict", separator="\n"))async def triggerer(msg):	trigger["changed"] = {msg.to: True}	await cl.talk.sendMessage(msg.to, "Gimme image for change group picture.")	@cl.poll.hooks(type=25, filters=Filters.image)async def change_with_trigger(msg):	print(msg)	#check this group available for change pict	if msg.to in trigger["changed"]:		#check this group can changed		if trigger["changed"][msg.to] == True:			path = await cl.downloadObjMessage(msg.id)			await cl.updateGroupPicture(msg.to, path=path, remove_path=True)			await cl.talk.sendMessage(msg.to, "Success change group pictute")			loop = asyncio.get_event_loop()loop.run_until_complete(cl.poll.streams())
+from AsyncLine import *
+import asyncio
+
+cl = LineNext("ios")
+cl.login(name="syncline")
+
+cl.auth.url("/P4")
+
+trigger = {
+	"changed":{}
+}
+
+@cl.poll.hooks(type=25, filters=Filters.command("change profile", separator="\n"))
+async def change_profile_picture(msg):
+	"""
+	This method to change your profile picture
+	
+	Args used: path as string where image store
+	and then remove path after changing profile
+	"""
+	path = "LINE.png"
+	await cl.changeProfile(path=path, remove_path=True)
+	
+@cl.poll.hooks(type=25, filters=Filters.command("change group pict", prefix="", separator="\n"))
+async def change_profile_picture(msg):
+	"""
+	This method to change group picture
+	
+	Args used: url as string this url support content stream
+	and then remove path after changing profile
+	"""
+	url = "url image content"
+	await cl.updateGroupPicture(msg.to, url=path, remove_path=True)
+
+@cl.poll.hooks(type=25, filters=Filters.command("change pict", separator="\n"))
+async def triggerer(msg):
+	trigger["changed"] = {msg.to: True}
+	await cl.talk.sendMessage(msg.to, "Gimme image for change group picture.")
+	
+@cl.poll.hooks(type=25, filters=Filters.image)
+async def change_with_trigger(msg):
+	print(msg)
+	#check this group available for change pict
+	if msg.to in trigger["changed"]:
+		#check this group can changed
+		if trigger["changed"][msg.to] == True:
+			path = await cl.downloadObjMessage(msg.id)
+			await cl.updateGroupPicture(msg.to, path=path, remove_path=True)
+			await cl.talk.sendMessage(msg.to, "Success change group pictute")
+			
+loop = asyncio.get_event_loop()
+loop.run_until_complete(cl.poll.streams())
