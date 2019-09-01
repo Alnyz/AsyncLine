91b236c61d024a3bd927f24488c5fa0b4d2026bd
Alnyz <katro.coplax@gmail.com>
2019-09-01 21:25:58 +0700

    fix synchronized require install packaged

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
 import sys
+import pathlib
+
+try:
+    from pip.req import parse_requirements
+except ImportError:  # pip >= 10.0.0
+    from pip._internal.req import parse_requirements
+
+WORK_DIR = pathlib.Path(__file__).parent
+
 
 with open("README.md", encoding="utf-8") as f:
 	long_description = f.read()
@@ -9,17 +18,10 @@ MINIMAL_PY_VERSION = (3, 7)
 if sys.version_info < MINIMAL_PY_VERSION:
     raise RuntimeError('asyncline works only with Python {}+'.format('.'.join(map(str, MINIMAL_PY_VERSION))))
 
-install_requires = [
-	'frugal',
-	'rsa',
-	'ujson',
-	'requests',
-	'async_timeout',
-	'aiohttp',
-	'aiostomp',
-	'asyncio-nats-client'
-	'pymongo'
-]
+def install_requires():
+    file = WORK_DIR / "requirements.txt"
+    install_reqs = parse_requirements(str(file), session='sync')
+    return [str(ir.req) for ir in install_reqs]
 
 setup(
 	name='AsyncLine',
@@ -29,7 +31,7 @@ setup(
 	description='LINE Unofficial Python Library with Asyncio support and C++ Binding',
 	author="Anysz, Doodspav, Dyseo",
 	author_email="katro.coplax@gmail.com",
-	packages=find_packages(exclude("examples", "examples")),
+	packages=find_packages(exclude("examples")),
 	url="https://github.com/dyseo/AsyncLine",
 	download_url="https://github.com/dyseo/AsyncLine/releases/latest",
 	license='MIT',
